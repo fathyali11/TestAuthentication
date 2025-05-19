b7662c4df127319333bc0a620e97cfd891c6873b
fathy <fathy@FATHYALI>
2025-05-20 02:57:05 +0300

    add custom authorization filter

---
+++ TestAuthentication/CustomAuthorization/HasPermission.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TestAuthentication.CustomAuthorization;
+
+public class HasPermission(string permission) : AuthorizeAttribute, IAuthorizationFilter
+{
+    private readonly string _permission = permission;
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if(context.HttpContext.User.Identity is not { IsAuthenticated: true })
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        if (!context.HttpContext.User.HasClaim(c => c.Type == "Permission" && c.Value == _permission))
+        {
+            context.Result = new ForbidResult();
+            return;
+        }
+    }
+}
