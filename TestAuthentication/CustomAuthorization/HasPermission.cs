735fc02628700216b485b2aba18cdfb45741462b
fathy <fathy@FATHYALI>
2025-06-07 12:21:31 +0300

    check if permission is null

--- TestAuthentication/CustomAuthorization/HasPermission.cs
+++ TestAuthentication/CustomAuthorization/HasPermission.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using TestAuthentication.Constants.AuthoriaztionFilters;
 
 namespace TestAuthentication.CustomAuthorization;
 
@@ -9,13 +10,21 @@ public class HasPermission(string permission) : AuthorizeAttribute, IAuthorizati
     private readonly string _permission = permission;
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        if(context.HttpContext.User.Identity is not { IsAuthenticated: true })
+        if (string.IsNullOrWhiteSpace(_permission))
+        {
+            context.Result = new BadRequestObjectResult("Permission cannot be null or empty.");
+            return;
+        }
+
+        
+
+        if (context.HttpContext.User.Identity is not { IsAuthenticated: true })
         {
             context.Result = new UnauthorizedResult();
             return;
         }
 
-        if (!context.HttpContext.User.HasClaim(c => c.Type == "Permission" && c.Value == _permission))
+        if (!context.HttpContext.User.HasClaim(c => c.Type == AdminRoleAndPermissions.Type && c.Value == _permission))
         {
             context.Result = new ForbidResult();
             return;
