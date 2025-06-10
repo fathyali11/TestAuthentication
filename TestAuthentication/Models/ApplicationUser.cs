d1492da3e47b8be9af3a9f395af868a7ef056fb1
fathy <fathy@FATHYALI>
2025-06-10 12:24:41 +0300

    add created at property for user

--- TestAuthentication/Models/ApplicationUser.cs
+++ TestAuthentication/Models/ApplicationUser.cs
@@ -7,4 +7,5 @@ public class ApplicationUser: IdentityUser
     public string Address { get; set; } = string.Empty;
     public string ProfilePictureUrl { get; set; } = string.Empty;
     public bool IsEnable { get; set; } = true;
+    public DateTime CreatedAt { get; set; }= DateTime.UtcNow;
 }
