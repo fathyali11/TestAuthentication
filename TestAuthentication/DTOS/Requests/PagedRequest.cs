010e5a713ea7f9418a6ab1a00431ee3b8750ac27
fathy <fathy@FATHYALI>
2025-06-27 17:14:18 +0300

    add pagination request

---
+++ TestAuthentication/DTOS/Requests/PagedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace UsersManagement.DTOS.Requests;
+
+public record PagedRequest(int PageIndex = PaginationConstants.DefaultPageIndex,
+    int PageSize = PaginationConstants.DefaultPageSize,
+    string? SearchTerm = null,
+    string? SortBy = null,
+    string? SortDirection = "asc"
+);
