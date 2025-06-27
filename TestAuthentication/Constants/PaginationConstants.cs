686e6d773fc1dfacfb86f872ca2e1768238b1fe0
fathy <fathy@FATHYALI>
2025-06-27 17:13:48 +0300

    add pagination constants

---
+++ TestAuthentication/Constants/PaginationConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace UsersManagement.Constants;
+
+public static class PaginationConstants
+{
+    public const int DefaultPageSize = 10;
+    public const int DefaultPageIndex = 1;
+    public const string UserNameSort = "UserName";
+    public const string EmailSort = "Email";
+    public const string CreatedAtSort = "CreatedAt";
+    public const string SortDirectionAsc = "asc";
+
+}
